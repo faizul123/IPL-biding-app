17e532701572d732c3ef8d14a146b011b4708a51
= <=>
2018-01-15 20:32:10 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Banner from './Banner'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Banner bannerImgURL="vivo-ipl-2018-1.jpg" />, document.getElementById('root'));
+registerServiceWorker();
