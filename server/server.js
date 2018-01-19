cdc7bd5ed3f0c066fc04862f8374fa1bf295b08a
= <=>
2018-01-19 12:57:03 +0530

    react index.html not found error fixed

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.post('/api/user/signin',userController.signin);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'../client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
